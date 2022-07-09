10e213e1dbe191e1c64725e35301efa535e89efe
fathonialimukti <50938757+fathonialimukti@users.noreply.github.com>
2022-07-09 14:49:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+    res.send('Express server ready');
+});
+
+
